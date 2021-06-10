c3d0f9ec7036c5cf077bca20f3c65f501c78b2f5
Allysht <81968946+Allysht@users.noreply.github.com>
2021-06-10 14:07:50 +0300

    Add files via upload

---
+++ background changer/script.js
@@ -0,0 +1,16 @@
+function getNewColor() {
+    var symbols, color;
+    symbols = "0123456789ABCDEF";
+
+    color = "#";
+
+    for (var i = 0; i < 6; i++) {
+        color = color + symbols[Math.floor(Math.random() * 16 )];
+    }
+
+    document.body.style.background = color;
+
+    document.getElementById("hex").innerHTML = color;
+}
+
+setInterval(getNewColor, 1000);
